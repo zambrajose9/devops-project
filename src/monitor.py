444a3ad59ea1691480be24e84224c76d921b232e
zambrajose9 <zambrajose9@gmail.com>
2024-09-28 10:46:16 -0300

    comienzo

--- src/monitor.py
+++
@@ -1,9 +0,0 @@
-from prometheus_client import Counter, start_http_server
-
-REQUESTS = Counter('prediction_requests', 'Number of prediction requests')
-
-def start_monitoring():
-    start_http_server(8001)
-
-def log_request():
-    REQUESTS.inc()
